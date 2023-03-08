143c7d81f1ee2447b9f4d5e57114527ed61f5eef
Tuyên Nguyễn <tts-tuyennd@amaisoft.com>
2023-03-08 08:40:14 +0700

    third times

--- routes/web.php
+++ routes/web.php
@@ -31,37 +31,29 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
-Route::group([
-    'name' => 'staff.'
-], function () {
-    Route::get('/create-staff-form', [StaffController::class, 'showCreate'])->name('create.form');
-    Route::get('/{staff}/edit-staff-form', [StaffController::class, 'showEdit'])->name('update.form');
-    Route::post('/create-staff', [StaffController::class, 'create'])->name('create');
-    Route::post('/{staff}/edit-staff', [StaffController::class, 'update'])->name('update');
-    Route::delete('/{staff}/delete-staff', [StaffController::class, 'delete'])->name('delete');
-});
+    Route::get('/create-staff-form', [StaffController::class, 'showCreate'])->name('staff.create.form');
+    Route::get('/{staff}/edit-staff-form', [StaffController::class, 'showEdit'])->name('staff.update.form');
+    Route::post('/create-staff', [StaffController::class, 'create'])->name('staff.create');
+    Route::post('/{staff}/edit-staff', [StaffController::class, 'update'])->name('staff.update');
+    Route::delete('/{staff}/delete-staff', [StaffController::class, 'delete'])->name('staff.delete');
 
-Route::group([
-    'name' => 'position.'
-], function () {
-    Route::get('/show-position', [PositionController::class, 'show'])->name('show');
-    Route::get('/create-position-form', [PositionController::class, 'showCreate'])->name('create.form');
-    Route::post('/create-position', [PositionController::class, 'create'])->name('create');
-    Route::get('/{position}/edit-position-form', [PositionController::class, 'showEdit'])->name('update.form');
-    Route::post('/{position}/edit-position', [PositionController::class, 'update'])->name('update');
-    Route::delete('/{position}/delete-position', [PositionController::class, 'delete'])->name('delete');
-});
 
-Route::group([
-    'name' => 'department.'
-], function () {
-    Route::get('/show-department', [DepartmentController::class, 'show'])->name('show');
-    Route::get('/create-department-form', [DepartmentController::class, 'showCreate'])->name('create.form');
-    Route::post('/create-department', [DepartmentController::class, 'create'])->name('create');
-    Route::get('/{department}/edit-department-form', [DepartmentController::class, 'showEdit'])->name('update.form');
-    Route::post('/{department}/edit-department', [DepartmentController::class, 'update'])->name('update');
-    Route::delete('/{department}/delete-department', [DepartmentController::class, 'delete'])->name('delete');
-});
+    Route::get('/show-position', [PositionController::class, 'show'])->name('position.show');
+    Route::get('/create-position-form', [PositionController::class, 'showCreate'])->name('position.create.form');
+    Route::post('/create-position', [PositionController::class, 'create'])->name('position.create');
+    Route::get('/{position}/edit-position-form', [PositionController::class, 'showEdit'])->name('position.update.form');
+    Route::post('/{position}/edit-position', [PositionController::class, 'update'])->name('position.update');
+    Route::delete('/{position}/delete-position', [PositionController::class, 'delete'])->name('position.delete');
+
+
+
+    Route::get('/show-department', [DepartmentController::class, 'show'])->name('department.show');
+    Route::get('/create-department-form', [DepartmentController::class, 'showCreate'])->name('department.create.form');
+    Route::post('/create-department', [DepartmentController::class, 'create'])->name('department.create');
+    Route::get('/{department}/edit-department-form', [DepartmentController::class, 'showEdit'])->name('department.update.form');
+    Route::post('/{department}/edit-department', [DepartmentController::class, 'update'])->name('department.update');
+    Route::delete('/{department}/delete-department', [DepartmentController::class, 'delete'])->name('department.delete');
+
 
 
 
